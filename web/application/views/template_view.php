cd1e01e6efad8fbfb07ed4758ffc07a6c7cff7a3
Nikonenko <mexahik3333@gmail.com>
2016-04-25 11:08:39 +0800

    создан файл template_view.php шаблон, содержащий общую для всех страниц разметку

---
+++ web/application/views/template_view.php
@@ -0,0 +1,17 @@
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <title>Главная</title>
+</head>
+<body>
+<?php include 'application/views/'.$content_view; ?>
+</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nikonenko
+ * Date: 25.04.2016
+ * Time: 10:54
+ */
